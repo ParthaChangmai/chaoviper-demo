df1bedef5190aea663db523db8672c5329a52ff1
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-29 04:41:29 +0530

    fixing the icons padding

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,29 +16,45 @@ const Skills = () => {
 					</p>
 					<p className="py-4">These are the technologies I've worked with </p>
 				</div>
-				<div className="w-full grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-10 text-center py-8">
+				<div className="w-full grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-20 text-center py-8 px-8">
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={HTML} alt="HTML icon" />
+						<img className="w-20 mx-auto md:pt-5 " src={HTML} alt="HTML icon" />
 						<p className="my-4">HTML</p>
 					</div>
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={CSS} alt="HTML icon" />
+						<img className="w-20 mx-auto md:pt-5" src={CSS} alt="HTML icon" />
 						<p className="my-4">CSS</p>
 					</div>
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={JavaScript} alt="HTML icon" />
+						<img
+							className="w-20 mx-auto md:pt-5"
+							src={JavaScript}
+							alt="HTML icon"
+						/>
 						<p className="my-4">Java Script</p>
 					</div>
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={ReactImg} alt="HTML icon" />
+						<img
+							className="w-20 mx-auto md:pt-5"
+							src={ReactImg}
+							alt="HTML icon"
+						/>
 						<p className="my-4">ReactJs</p>
 					</div>
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={GitHub} alt="HTML icon" />
+						<img
+							className="w-20 mx-auto md:pt-5"
+							src={GitHub}
+							alt="HTML icon"
+						/>
 						<p className="my-4">GitHub</p>
 					</div>
 					<div className="shadow-md md:shadow-white hover:scale-110 duration-500">
-						<img className="w-20 mx-auto " src={Tailwind} alt="HTML icon" />
+						<img
+							className="w-20 mx-auto md:pt-5"
+							src={Tailwind}
+							alt="HTML icon"
+						/>
 						<p className="my-4">Tailwind</p>
 					</div>
 				</div>

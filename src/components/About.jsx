f0a0cd74d20a0fbb926e43a31db0fe9140cc59ec
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-29 05:35:28 +0530

    motion additon

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import AnimationPage from '../animation/AnimationPage';
 
 const About = () => {
 	return (
-		<div name="about" className="w-full h-screen bg-black text-gray-300 ">
-			<div className="flex flex-col justify-center items-center w-full h-full">
-				<div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
-					<div className="sm:text-right pb-8 sm:pl-4">
-						<p className="text-4xl font-bold inline border-b-4 border-[#6133f5]">
-							About
-						</p>
+		<AnimationPage>
+			<div name="about" className="w-full h-screen bg-black text-gray-300 ">
+				<div className="flex flex-col justify-center items-center w-full h-full">
+					<div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
+						<div className="sm:text-right pb-8 sm:pl-4">
+							<p className="text-4xl font-bold inline border-b-4 border-[#6133f5]">
+								About
+							</p>
+						</div>
+						<div></div>
 					</div>
-					<div></div>
-				</div>
-				<div className="max-w-[1000px] w-full px-4 grid sm:grid-cols-2 gap-8 px-4">
-					<div className="sm:text-right text-4xl font-bold">
-						<p>Hi, I am Partha, Nice to meet you.Please take a look around</p>
-					</div>
-					<div>
-						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
-						similique molestias tempore eligendi reiciendis hic non autem odio.
-						Maiores aperiam dolor magni autem. Eum vitae quidem ullam, corrupti
-						optio in?
+					<div className="max-w-[1000px] w-full px-4 grid sm:grid-cols-2 gap-8 px-4">
+						<div className="sm:text-right text-4xl font-bold">
+							<p>Hi, I am Partha, Nice to meet you.Please take a look around</p>
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+							similique molestias tempore eligendi reiciendis hic non autem
+							odio. Maiores aperiam dolor magni autem. Eum vitae quidem ullam,
+							corrupti optio in?
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</AnimationPage>
 	);
 };
 

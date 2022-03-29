9993338c37a42605e50e530c168eca68e6753275
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-29 05:36:13 +0530

    motion additon to both pc and mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import AnimationPage from '../animation/AnimationPage';
 
 import pc from '../assets/pc.png';
 import Social from './Social';
@@ -10,61 +11,63 @@ const Navbar = () => {
 	const handleClick = () => setNav(!nav);
 
 	return (
-		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#171717] text-gray-300">
-			<div className="hover:cursor-pointer">
-				<Link to="/">
-					<img src={pc} alt="logo" style={{ width: '80px' }} />
-				</Link>
+		<AnimationPage>
+			<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#171717] text-gray-300">
+				<div className="hover:cursor-pointer">
+					<Link to="/">
+						<img src={pc} alt="logo" style={{ width: '80px' }} />
+					</Link>
+				</div>
+				{/* menu */}
+				<ul className="hidden md:flex ">
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="about">About</Link>
+					</li>
+					<li>
+						<Link to="skills">Skills</Link>
+					</li>
+					<li>
+						<Link to="work">Work</Link>
+					</li>
+					<li>
+						<Link to="contact">Contact</Link>
+					</li>
+				</ul>
+				{/* Hamburger */}
+				<div onClick={handleClick} className="md:hidden z-10">
+					{!nav ? <FaBars /> : <FaTimes />}
+				</div>
+				{/*  Mobile menu  */}
+				<ul
+					className={
+						!nav
+							? 'hidden'
+							: 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'
+					}
+				>
+					<li onClick={handleClick} className="py-6 text-4xl">
+						<Link to="/">Home</Link>
+					</li>
+					<li onClick={handleClick} className="py-6 text-4xl">
+						<Link to="about">About</Link>
+					</li>
+					<li onClick={handleClick} className="py-6 text-4xl">
+						<Link to="skills">Skills</Link>
+					</li>
+					<li onClick={handleClick} className="py-6 text-4xl">
+						<Link to="work">Work</Link>
+					</li>
+					<li onClick={handleClick} className="py-6 text-4xl">
+						<Link to="contact">Contact</Link>
+					</li>
+				</ul>
+				{/* Social icons*/}
+				<Social />
 			</div>
-			{/* menu */}
-			<ul className="hidden md:flex ">
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="about">About</Link>
-				</li>
-				<li>
-					<Link to="skills">Skills</Link>
-				</li>
-				<li>
-					<Link to="work">Work</Link>
-				</li>
-				<li>
-					<Link to="contact">Contact</Link>
-				</li>
-			</ul>
-			{/* Hamburger */}
-			<div onClick={handleClick} className="md:hidden z-10">
-				{!nav ? <FaBars /> : <FaTimes />}
-			</div>
-			{/*  Mobile menu  */}
-			<ul
-				className={
-					!nav
-						? 'hidden'
-						: 'absolute top-0 left-0 w-full h-screen bg-black flex flex-col justify-center items-center'
-				}
-			>
-				<li className="py-6 text-4xl">
-					<a href="/">Home</a>
-				</li>
-				<li className="py-6 text-4xl">
-					<a href="/about">About</a>
-				</li>
-				<li className="py-6 text-4xl">
-					<a href="/skills">Skills</a>
-				</li>
-				<li className="py-6 text-4xl">
-					<a href="/work">Work</a>
-				</li>
-				<li className="py-6 text-4xl">
-					<a href="/contact">Contact</a>
-				</li>
-			</ul>
-			{/* Social icons*/}
-			<Social />
-		</div>
+		</AnimationPage>
 	);
 };
 

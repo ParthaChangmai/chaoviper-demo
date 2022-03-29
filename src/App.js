647c7cd08e358ccb2f2419904ad230195d70a0ad
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-03-29 05:35:18 +0530

    added animatePresence and key to routes

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import Home from './components/Home';
 import Navbar from './components/Navbar';
 import Skills from './components/Skills';
 import Work from './components/Work';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
 
 function App() {
+	const location = useLocation();
+
 	return (
 		<>
-			<Navbar />
+			<AnimatePresence exitBeforeEnter initial={false}>
+				<Navbar />
 
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/skills" element={<Skills />} />
-				<Route path="/work" element={<Work />} />
-				<Route path="/contact" element={<Contacts />} />
-			</Routes>
+				<Routes key={location.pathname} location={location}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/skills" element={<Skills />} />
+					<Route path="/work" element={<Work />} />
+					<Route path="/contact" element={<Contacts />} />
+				</Routes>
+			</AnimatePresence>
 
 			{/* <Home />
 			<About />

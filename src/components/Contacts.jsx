38b197bfa5a156b287e8bbba13bcc95dcbc2639a
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-04-18 09:23:33 +0530

    small mobile ui fix

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -13,7 +13,7 @@ const Contacts = () => {
 					action="https://getform.io/f/4e603c32-fc17-4e50-b5d7-1357a9b3b94d"
 					className="flex flex-col max-w-[600px] w-full"
 				>
-					<div className="pb-8">
+					<div className="pb-8 pt-40 sm:pt-10">
 						<p className="text-4xl font-bold inline border-b-4 border-[#6133f5] text-gray-300">
 							Contact
 						</p>

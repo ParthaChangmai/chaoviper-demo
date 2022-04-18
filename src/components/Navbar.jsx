2e6221890e779d19265a538c579a3b211e0fc57e
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-04-18 09:01:01 +0530

    added theme button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,91 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
 import { Link, NavLink } from 'react-router-dom';
+import { GiNightSky } from 'react-icons/gi';
+import { MdModeNight } from 'react-icons/md';
 
-import pc from '../assets/pc.png';
+import { themeChange } from 'theme-change';
+
 import Social from './Social';
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
+	const [btn, setBtn] = useState(false);
+	const [navbar, setNavbar] = useState(false);
 	const handleClick = () => setNav(!nav);
 
-	let activeClassName = 'border-b-4 border-[#6133f5]';
+	const handleBtnAndNavbar = () => {
+		setBtn(!btn);
+		setNavbar(!navbar);
+	};
+
+	let activeClassName =
+		' tab tab-lifted tab-active hover:text-white text-white';
+	const nonActiveClass =
+		'hover:text-white tab tab-lifted hover:tab-active text-white';
+
+	const navBarBlack =
+		'fixed w-full h-[80px] flex justify-between items-center px-4  bg-[#171717] ';
+	const navBarBlue =
+		'fixed w-full h-[80px] flex justify-between items-center px-4  bg-[#182647] ';
+
+	useEffect(() => {
+		const themeData = window.localStorage.getItem('theme-data');
+		const savedValues = JSON.parse(themeData);
+		setBtn(savedValues.btn);
+		setNavbar(savedValues.navbar);
+	}, []);
+
+	useEffect(() => {
+		const valuesToSave = { btn, navbar };
+		window.localStorage.setItem('theme-data', JSON.stringify(valuesToSave));
+	});
+
+	useEffect(() => {
+		themeChange(false);
+	}, []);
 
 	return (
-		<div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#171717] text-gray-300">
-			<div className="hover:cursor-pointer">
-				<Link to="/">
-					<img src={pc} alt="logo" style={{ width: '80px' }} />
-				</Link>
+		<div className={!navbar ? navBarBlue : navBarBlack}>
+			<div className="hover:cursor-pointer ">
+				<div
+					onClick={handleBtnAndNavbar}
+					className="text-2xl sm:text-4xl md:pl-10"
+				>
+					{!btn ? (
+						<button
+							data-toggle-theme="night,black"
+							data-act-class="ACTIVECLASS"
+						>
+							<GiNightSky />
+						</button>
+					) : (
+						<button
+							data-toggle-theme="night,black"
+							data-act-class="ACTIVECLASS"
+						>
+							<MdModeNight />
+						</button>
+					)}
+				</div>
 			</div>
 			{/* menu */}
 			<ul className="hidden md:flex ">
 				<li>
 					<NavLink
 						className={({ isActive }) =>
-							isActive ? activeClassName : 'hover:border-b-4 border-[#6133f5]'
+							isActive ? activeClassName : nonActiveClass
 						}
 						to="/"
 					>
 						Home
 					</NavLink>
 				</li>
+
 				<li>
 					<NavLink
 						className={({ isActive }) =>
-							isActive ? activeClassName : 'hover:border-b-4 border-[#6133f5]'
+							isActive ? activeClassName : nonActiveClass
 						}
 						to="about"
 					>
@@ -43,7 +95,7 @@ const Navbar = () => {
 				<li>
 					<NavLink
 						className={({ isActive }) =>
-							isActive ? activeClassName : 'hover:border-b-4 border-[#6133f5]'
+							isActive ? activeClassName : nonActiveClass
 						}
 						to="skills"
 					>
@@ -56,7 +108,7 @@ const Navbar = () => {
 				<li>
 					<NavLink
 						className={({ isActive }) =>
-							isActive ? activeClassName : 'hover:border-b-4 border-[#6133f5]'
+							isActive ? activeClassName : nonActiveClass
 						}
 						to="contact"
 					>
